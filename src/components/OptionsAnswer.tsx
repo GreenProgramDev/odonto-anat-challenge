c6d715bb8ed97b3e0d655581a4185e7ce66b730f
GreenDev <greenprogramdev@gmail.com>
2023-10-21 04:30:22 -0300

    feat: created component for quests from quiz

---
+++ src/components/OptionsAnswer.tsx
@@ -0,0 +1,22 @@
+'use client'
+import Image from "next/image";
+
+interface IOptionsAnswer {
+    children?: string;
+    name?: string,
+    // response?: {array.Correct ? correct : wrong},
+}
+
+const OptionsAnswer = (props:IOptionsAnswer) => { 
+
+    return (
+        <div className="flex justify-between text-cyan-50">
+            <label className="flex gap-2">
+                <input  type="radio" name={props.name} />
+                {props.children}
+            </label>
+        </div>
+    )
+}
+
+export default OptionsAnswer
