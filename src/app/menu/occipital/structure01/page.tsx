97b87077ef51ba1541305a0d8c0221ae5a73c649
Sidney Lima <greenprogramdev@gmail.com>
2023-10-28 11:41:53 -0300

    feat: creat function that change the questions within the quiz

--- src/app/menu/occipital/structure01/page.tsx
+++ src/app/menu/occipital/structure01/page.tsx
@@ -6,44 +6,73 @@ import ButtonBack from '@/components/ButtonBack';
 import Link from 'next/link';
 import generateQuestions from '@/app/utils/helper/generateQuestions';
 import namesBones from '@/app/utils/constants/namesBones';
+import Counter from '@/components/Counter';
+import { useState } from 'react';
 
 
 export default function Structure01() {
 
-    let quests = generateQuestions(namesBones, "temporal")
+    const [structureIndex, setStructureIndex] = useState(0)
+    
+    let structure = [
+        generateQuestions(namesBones, "test1"),
+        generateQuestions(namesBones, "test2"),
+        generateQuestions(namesBones, "test3"),
+        generateQuestions(namesBones, "test4"),
+        generateQuestions(namesBones, "test5"),
+        generateQuestions(namesBones, "test6"),
+        generateQuestions(namesBones, "test7"),
+        generateQuestions(namesBones, "test8"),
+        generateQuestions(namesBones, "test9"),
+        generateQuestions(namesBones, "test10"),
+        generateQuestions(namesBones, "test11"),
+    ]
+
+    
+
+   const handleNextQuest = () => {
+
+    if(structureIndex < structure.length - 1){
+        setStructureIndex(structureIndex + 1)
+
+    }
+
+    const rightAnswerInput = document.querySelector('input[name="option"]:checked.rightAnswer')
+
+    if(rightAnswerInput){
+        
+    }
+
+
+   }
+
+   const structureTitle = `Structure ${structureIndex + 1}`
+   const nextButtonText = structureIndex === structure.length - 1 
+   ?   <Link className="" href="/menu/occipital/structure02">Next Quiz</Link> 
+   : "Next Quest"
+
+
+   
     return (
         <>
             <div className=" bg-black h-screen flex flex-col items-center ">
             <form >
                 <Image className='w-screen ' src={PageOne} alt="img" />
             {/* the tag h2 received component what render each structure*/}
-                <h2 className='text-red-50 text-lg text-center my-5'>Structure 01</h2>
+                <h2 className='text-red-50 text-lg text-center my-5'>{structureTitle}</h2>
                 <div className='flex justify-around items-center px-5'>
                     <div className='mr-10 pr-5'>
             {/* options from quest received component what render four wrong and one correct*/}
-            {quests.map((quest) => (
-                <OptionsAnswer key={quest} name='option'>{quest}</OptionsAnswer>
-            ))}
-   
+
+            <div>
+                {structure[structureIndex].map( (quest) => (<OptionsAnswer key={quest} name='option' >{quest}</OptionsAnswer>))}
+            </div>
                        
-                    </div>
-                    <button className='text-cyan-50      my-6 p-3  h-5  bg-zinc-700 flex items-center text-center border-b-2 border-r-2 rounded-lg'  type='button'>Next Quest</button>
-                </div>
-
-                <div className='bg-slate-800 flex flex-col pb-2 mx-16 my-10 border-b-4 rounded-xl'>
-                    <p className='text-cyan-50 flex justify-center mt-5'>Counter</p>
-                    <div className='flex justify-around px-2 py-3'>
-                        <div>
-                            <h1 className='text-cyan-50 flex justify-center' >Correct</h1>
-                            <p className='text-cyan-50 flex justify-center' >0</p>
-                        </div>
-                        <div>
-                            <h1 className='text-cyan-50 flex justify-center' >Wrong</h1>
-                            <p className='text-cyan-50 flex justify-center' >0</p>
-                        </div>
-                    </div>
-                </div>
-
+            </div>
+                <button className='text-cyan-50      my-6 p-3  h-5  bg-zinc-700 flex items-center text-center border-b-2 border-r-2 rounded-lg' onClick={handleNextQuest} type='button'>{nextButtonText}</button>
+            </div>
+            
+            <Counter />
             
             </form> 
             <div className='fixed bottom-5 flex gap-16 pt-10'>
