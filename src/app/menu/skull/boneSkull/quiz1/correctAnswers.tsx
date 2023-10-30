cc9a4d3095897381271e68385f608d29a0803261
Sidney Lima <greenprogramdev@gmail.com>
2023-10-30 09:51:46 -0300

    chore: update answerCorrect

--- src/app/menu/skull/boneSkull/quiz1/correctAnswers.tsx
+++ src/app/menu/skull/boneSkull/quiz1/correctAnswers.tsx
@@ -2,17 +2,32 @@ import generateQuestions from '@/app/utils/helper/generateQuestions';
 import namesBones from '@/app/utils/constants/namesBones';
 
 export const correctAnswers: string[] = [
-    "Face Temporal",
-    "Poro acústico externo",
-    "Forame mastóideo",
-    "Meato acústico externo",
-    "Fissura timpanomastóidea",
-    "Proc. mastóide",
-    "Proc. estilóide",
-    "Sulcos arteriais",
-    "Fossa mandibular",
-    "Poro acústico externo",
-    "Proc. zigomático"
+    "Osso frontal",
+    "Osso nasal",
+    "Lamina orbital do osso etmoide",
+    "Sutura coronal",
+    "Osso pariental",
+    "Proce. frontal do osso zigo.",
+    "Asa maior do osso esfenoide",
+    "Superf. orbital do osso zigo.",
+    "Superf. orbital da maxila",
+    "Proce. zigo. da maxila",
+    "Forame infraorbital",
+    "Forame mentual",
+    "Corpo da mandibula",
+    "Proce. alveolar da maxila",
+    "Espinha nasal int. da maxima",
+    "Ramo da mandíbula",
+    "Osso etmoide",
+    "Osso zigomático",
+    "Proce. temporal do osso zigo.",
+    "Osso esfenoide",
+    "Processo frontal da maxila",
+    "Osso temporal",
+    "Asa menor do osso esfenoide",
+    "Superf. orbital do osso front",
+    "Forame supraorbital",
+    "Glabela do osso frontal",
 ];
 
 export const structure = correctAnswers.map((correctAnswer: string) => generateQuestions(namesBones, correctAnswer));
