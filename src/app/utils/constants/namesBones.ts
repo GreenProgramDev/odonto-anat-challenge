7cb9593e930dd4ceb40f3645a7892289e3226930
Sidney Lima <greenprogramdev@gmail.com>
2023-10-31 13:10:40 -0300

    chore: update names

--- src/app/utils/constants/namesBones.ts
+++ src/app/utils/constants/namesBones.ts
@@ -24,7 +24,7 @@ const namesBones = [
     "Hâmulo Pterigóideo",
     "Fissura Orbital Superior",
     "Crista Esfenoidal",
-    "Abertura Seio Esfenoidal",
+    "Abert. Seio Esfenoidal",
     "Asa Menor",
     "Asa Maior",
     "Forame Redondo",
