ddefe99b772c8d1a47a9a8f4d5140b860b2e2b17
Sidney Lima <greenprogramdev@gmail.com>
2023-11-01 05:12:03 -0300

    chore: update for add quiz2 and quiz3

--- src/app/menu/skull/boneSkull/page.tsx
+++ src/app/menu/skull/boneSkull/page.tsx
@@ -1,5 +1,7 @@
 import QuizLink from '@/components/QuizLink'
 import PagA2 from '../../../../../public/assets/imgBoneSkull/pagA1.svg' 
+import PagB2 from '../../../../../public/assets/imgBoneSkull/pagB1.svg' 
+import PagC2 from '../../../../../public/assets/imgBoneSkull/pagC1.svg' 
  
 
 export default function BoneSkullGroup(){
@@ -12,6 +14,8 @@ export default function BoneSkullGroup(){
                 <div className='grid grid-cols-3  gap-x-3 gap-y-2 py-1 justify-center '>                            
                                  {/* temporary files for illustration */}                            
                     <QuizLink src={PagA2} href="/menu/skull/boneSkull/quiz1">Quiz 01</QuizLink>
+                    <QuizLink src={PagB2} href="/menu/skull/boneSkull/quiz2">Quiz 02</QuizLink>
+                    <QuizLink src={PagC2} href="/menu/skull/boneSkull/quiz3">Quiz 03</QuizLink>
                 </div>   
             </div>
         </>
