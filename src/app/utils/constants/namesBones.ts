72b46b5a883c24cb9531f0c95bcedaed356b3c81
Sidney Lima <greenprogramdev@gmail.com>
2023-11-01 05:09:03 -0300

    chore: add more names of bones

--- src/app/utils/constants/namesBones.ts
+++ src/app/utils/constants/namesBones.ts
@@ -59,7 +59,24 @@ const namesBones = [
     "Processo Frontal da Maxila",
     "Osso Temporal",
     "Forame Supraorbital",
-    "Glabela Osso Frontal,"
+    "Glabela Osso Frontal",
+    "Linha Temporal Superior",
+    "Sutura Escamosa",
+    "Meato Acúst. Ext. O. Temp.",
+    "Part Petrosa Osso Temp.",
+    "Osso Occipital",
+    "Proce. Mastoide O. Temp.",
+    "Proce. Estiloide O. Temp.",
+    "Arco Zigomático",
+    "Proce. Zigo. Osso Temp.",
+    "Part Escamosa Osso Temp.",
+    "Fossa Temporal",
+    "Sutura Esfenopariental",
+    "Linha Temporal Inferior",
+    "Sutura Sagital",
+    "Sutura Lambdóide",
+    "Forame Parietal",
+    "Osso Parietal",
 ];
 
 
