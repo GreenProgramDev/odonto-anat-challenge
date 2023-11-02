b57e34fb480ab6e33e5799f502d77ebc4db3d2c1
Sidney Lima <greenprogramdev@gmail.com>
2023-11-02 10:19:06 -0300

    chore: update router of login

--- src/app/menu/skull/boneSkull/page.tsx
+++ src/app/menu/skull/boneSkull/page.tsx
@@ -12,7 +12,7 @@ export default function BoneSkullGroup(){
             <div className='mt-3 border-x-2 p-2 text-center rounded-xl'>
                 
                 <h2 className=' text-cyan-50'>Skull Bones View</h2>
-                <div className='grid grid-cols-3  gap-x-3 gap-y-2 py-1 justify-center '>                            
+                <div className='grid grid-cols-3  gap-x-1 gap-y-1 py-1 justify-center '>                            
                                  {/* temporary files for illustration */}                            
                     <QuizLink src={PagA1} href="/menu/skull/boneSkull/quiz1"></QuizLink>
                     <QuizLink src={PagB1} href="/menu/skull/boneSkull/quiz2"></QuizLink>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,28 +4,25 @@ import Image from 'next/image'
 import LogoImg from '../../public/assets/logo.svg' 
 import Button from '@/components/Button'
 import Menu from './menu/skull/page'
+import { LoginCard } from '@/components/LoginCard'
+import Login from './account/page'
 
 export default function MyApp() {
 
-    const [showMenu, setShowMenu] = useState(false)
-
-    const handleBtnMenu = () => {
-        setShowMenu(!showMenu)
+    
+      const [showLogins, setShowLogins] = useState(false)
+    
+    const handleBtnLogin = () => {
+        setShowLogins(false)
+    }
+    const handleBtnRegister = () => {
+        setShowLogins(true)
     }
 
     return (
     <main className="bg-black" >
         <>
-            {!showMenu ? (
-                <div className={` gap-10 flex flex-col justify-center items-center w-screen h-screen `}>
-                <Image className='w-60' src={LogoImg} alt="logotipo" />
-                <Button onClick={handleBtnMenu}>Menu</Button>
-            </div>
-            ):(
-                <div className={` flex flex-col justify-center items-center `}>
-                <Menu/>
-            </div>
-            )}
+        <Login/>
         </>
     </main>
 
